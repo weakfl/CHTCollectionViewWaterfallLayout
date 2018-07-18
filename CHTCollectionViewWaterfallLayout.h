a5086757fea8f06b55e8738360b3d4c5074b1620
weak <weak@fraglab.at>
2018-07-18 14:43:53 +0200

    adapt for personal use, add sticky header and improve performance

--- CHTCollectionViewWaterfallLayout.h
+++ CHTCollectionViewWaterfallLayout.h
@@ -11,9 +11,9 @@
  *  Enumerated structure to define direction in which items can be rendered.
  */
 typedef NS_ENUM (NSUInteger, CHTCollectionViewWaterfallLayoutItemRenderDirection) {
-  CHTCollectionViewWaterfallLayoutItemRenderDirectionShortestFirst,
-  CHTCollectionViewWaterfallLayoutItemRenderDirectionLeftToRight,
-  CHTCollectionViewWaterfallLayoutItemRenderDirectionRightToLeft
+    CHTCollectionViewWaterfallLayoutItemRenderDirectionShortestFirst,
+    CHTCollectionViewWaterfallLayoutItemRenderDirectionLeftToRight,
+    CHTCollectionViewWaterfallLayoutItemRenderDirectionRightToLeft
 };
 
 /**
@@ -321,5 +321,8 @@ extern NSString *const CHTCollectionElementKindSectionFooter;
  *    The width of an item is calculated based on number of columns, the collection view width, and the horizontal insets for that section.
  */
 - (CGFloat)itemWidthInSectionAtIndex:(NSInteger)section;
+
+@property (weak, nonatomic) NSIndexPath *indexPath;
+@property (nonatomic) BOOL shouldInvalidateLayout;
 
 @end
